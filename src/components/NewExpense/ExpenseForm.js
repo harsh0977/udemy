06370d7f4b85757f26af4aaa0eabcacf5d292d6d
Harsh077 <hpjio8849@gmail.com>
2022-07-07 12:06:01 +0530

    validation updated

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -8,6 +8,9 @@ const ExpenseForm = (props) => {
     const [a,setA] = useState('');
     const [d,setD] = useState('');
 
+    const [isValid1, setIsValid1] = useState(true);
+    const [isValid2, setIsValid2] = useState(true);
+    const [isValid3, setIsValid3] = useState(true);
     // const [user,setUser] = useState({
     //     title:'',
     //     amount:'',
@@ -15,8 +18,11 @@ const ExpenseForm = (props) => {
     // })
 
     const fun1 = (event) =>{
+        
+        if (event.target.value.trim().length > 0) {
+          setIsValid1(true);
+        }
         setT(event.target.value)
-
         // setUser({
         //     ...user,
         //     title:event.target.value
@@ -28,27 +34,49 @@ const ExpenseForm = (props) => {
     }
 
     const fun2 = (event) =>{
+      if (event.target.value.trim().length > 0) {
+        setIsValid2(true);
+      }
         setA(event.target.value)
     }
 
     const fun3 = (event) =>{
+      if (event.target.value.trim().length > 0) {
+        setIsValid3(true);
+      }
         setD(event.target.value)
     }
 
     const submit = (event) =>{
         event.preventDefault();
 
-        const expenseData= {
-            title:t,
-            amount:a,
-            date:new Date(d)
+        if (t.trim().length ===  0 )
+        {
+          setIsValid1(false);
+        }
+        if (a.trim().length ===  0 )
+        {
+          setIsValid2(false);
+        }
+        if (d.trim().length ===  0 )
+        {
+          setIsValid3(false);
+        }
+        if(isValid1 === true && isValid2 === true &&isValid3 === true)
+        {
+              const expenseData= {
+                title:t,
+                amount:a,
+                date:new Date(d)
+            }
+            
+            props.onSave(expenseData)
+            // console.log(expenseData)
+            setT('')
+            setA('')
+            setD('')
         }
         
-        props.onSave(expenseData)
-        // console.log(expenseData)
-        setT('')
-        setA('')
-        setD('')
 
     }
 
@@ -57,15 +85,22 @@ const ExpenseForm = (props) => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' value= {t} onChange={fun1}/>
+          <input type='text' value= {t} onChange={fun1}
+           style={{
+            borderColor: !isValid1 ? 'red' : 'white'
+           }} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' value= {a}  min='0.01' step='0.01' onChange={fun2} />
+          <input type='number' value= {a}  min='0.01' step='0.01' onChange={fun2} style={{
+            borderColor: !isValid2 ? 'red' : 'white'
+           }}/>
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' value= {d} min='2019-01-01' max='2022-12-31' onChange={fun3} />
+          <input type='date' value= {d} min='2019-01-01' max='2022-12-31' onChange={fun3} style={{
+          borderColor: !isValid3 ? 'red' : 'white'
+          }}/>
         </div>
       </div>
       <div className='new-expense__actions'>
